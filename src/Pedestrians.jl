8674eaa7678cf90b3030675a8f6fe44758b41375
VaclavMacha <vaclav.macha92@gmail.com>
2021-06-12 21:28:41 +0200

    Added Pedestrian type

--- src/Pedestrians.jl
+++ src/Pedestrians.jl
@@ -1,3 +1,27 @@
 module Pedestrians
 
+using Agents
+using LinearAlgebra
+using Random
+
+export Pedestrian
+
+# basic types
+Base.@kwdef mutable struct Pedestrian <: AbstractAgent
+    # mandatory properties
+    id::Int
+    pos::NTuple{2,Float64}
+    vel::NTuple{2,Float64} = (0., 0.)
+
+    # additional properties
+    target_id::Int      # id of the currenttarget
+    acc::Float64 = 0.5  # acceleration
+    radius::Float64 = 1 # pedestrian size
+    φ::Float64 = 3π/4   # maximum change of a pedestrian course
+end
+
+Pedestrian(id, pos, target_id; kwargs...) = Pedestrian(; id, pos, target_id, kwargs...)
+
+include("utilities.jl")
+
 end # module
