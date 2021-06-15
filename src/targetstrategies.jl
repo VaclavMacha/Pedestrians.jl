52edec28f1134917864b36f67efbbdbbad5dc0d8
VaclavMacha <vaclav.macha92@gmail.com>
2021-06-15 17:21:51 +0200

    Added parameters for Nearest type

--- src/targetstrategies.jl
+++ src/targetstrategies.jl
@@ -1,8 +1,11 @@
 abstract type TargetStrategy end
 
-struct Nearest <: TargetStrategy end
+Base.@kwdef struct Nearest <: TargetStrategy
+    p::Float64 = 0.5
+    dmin::Float64 = 1e-6
+end
 
-function find_target!(::Nearest, p::Pedestrian, model)
+function find_target!(s::Nearest, p::Pedestrian, model)
     pos = p.pos
     dist = Inf
     isexit = false
@@ -13,7 +16,7 @@ function find_target!(::Nearest, p::Pedestrian, model)
         p.pos[2] > t_pos[2] || continue
         
         d = distance(p.pos, t_pos)
-        if d < dist || d == dist && rand() > 0.5
+        if d < dist || abs(d - dist) <= s.dmin && rand() > s.p
             pos, dist = t_pos, d 
         end
     end
@@ -24,7 +27,7 @@ function find_target!(::Nearest, p::Pedestrian, model)
         p.pos[2] > t_pos[2] || continue
         
         d = distance(p.pos, t_pos)
-        if d < dist || d == dist && rand() > 0.5
+        if d < dist || abs(d - dist) <= s.dmin && rand() > s.p
             pos, dist, isexit = t_pos, d, true
         end
     end
