52c9b6760b038392e1d78bb5483f5c508de61117
VaclavMacha <vaclav.macha92@gmail.com>
2021-06-15 09:51:14 +0200

    Updated blind_velocity example

--- scripts/example.jl
+++ scripts/example.jl
@@ -2,20 +2,14 @@ using Revise
 
 using Pedestrians
 using Plots
-using LinearAlgebra
 
-target = (3, 0)
-positions = [(1, 2), (5, 4), (2, 1), (2, 3.5), (5, 1.5), (4.5, 2.5), (1, 4)]
+# create model
+k = 500
+model = build_model()
 
-ps = map(enumerate(positions)) do (i, pos)
-    vel = target .- pos
-    vel = 0.5 .* vel ./ norm(vel,2)
-    Pedestrian(i, pos, 1; vel)
-end
-
-r = Room()
-
-@time begin
-    plt = makeplot(r)
-    makeplot!(plt, ps; addview = true)
-end
+@time anim = @animate for i in 1:k
+    title = string("Blind velocity: ", lpad(i, 3, " "))
+    makeplot(model; title, addview = true)
+    simulation_step!(model)
+end;
+gif(anim, "./assets/blind_velocity.gif", fps = 10)
