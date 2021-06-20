e3e4a514a24cf8de1ba26a108f804ae76b795eba
VaclavMacha <vaclav.macha92@gmail.com>
2021-06-20 15:43:02 +0200

    Added initial velocity

--- src/strategies_time.jl
+++ src/strategies_time.jl
@@ -7,18 +7,20 @@ Basic time strategy that adds new pedestrian to all available entrances every `a
 
 - at::Int = 20
 - max_peds::Int = 300
+- vel_init::Point{<:Real} = initial velocity
 
 """
-Base.@kwdef struct Basic <: TimeStrategy
+Base.@kwdef struct Basic{T} <: TimeStrategy
     at::Int = 20
     max_peds::Int = 300
+    vel_init::Point{T} = (0.0, -1.3)
 end
 
 function add_pedestrian!(s::Basic, m::Model)
     if iter(m) == 0 || mod(iter(m), s.at) == 0
         for d in m.room.entrances
             maxid(m) >= s.max_peds && break
-            add_pedestrian!(m, Pedestrian(0, middle(d)))
+            add_pedestrian!(m, Pedestrian(0, middle(d); vel = s.vel_init))
         end
     end
     return
